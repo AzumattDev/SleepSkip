ab33e6773216b6ee2be021b8a6ba38bd5741656f
AzumattDev <azumattgaming@gmail.com>
2022-06-18 03:06:37 -0400

    Initial commit

---
+++ Patches.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SleepSkip;
+
+[HarmonyPatch(typeof(Game), nameof(Game.EverybodyIsTryingToSleep))]
+static class Game_EverybodyIsTryingToSleep_Patch
+{
+    static bool Prefix(Game __instance, ref bool __result)
+    {
+        // Amount of players in bed
+        int count = 0;
+
+        // Get all players
+        List<ZDO> allCharacterZdos = ZNet.instance.GetAllCharacterZDOS();
+        // Return false if none
+        if (allCharacterZdos.Count == 0)
+        {
+            __result = false;
+            return false;
+        }
+
+        // Count number of players in bed
+        foreach (ZDO zdo in allCharacterZdos)
+        {
+            if (zdo.GetBool("inBed"))
+                count++;
+        }
+
+        // Don't run the rest of the code if no one is in bed
+        /*if (count <= 0)
+        {
+            __result = false;
+            return false;
+        }*/
+
+        // Calculate current ratio of people sleeping
+        double sleepRatio = (double)count / allCharacterZdos.Count;
+
+        // If showMessage is true
+        if (SleepSkipPlugin.message.Value)
+        {
+            // If people are sleeping
+            if (count <= 1 || count >= 1)
+            {
+                SleepSkipPlugin.acceptedSleepCount = count;
+                SleepSkipPlugin.Dialog!.SetActive(true);
+
+                foreach (ZNetPeer instanceMPeer in ZNet.instance.m_peers)
+                {
+                    // Open menu on the client
+                    ZRoutedRpc.instance.InvokeRoutedRPC(instanceMPeer.m_characterID.m_userID, "OpenMenuOnClient");
+                    /*// Send message to everyone at everyone's position
+                    ZRoutedRpc.instance.InvokeRoutedRPC(instanceMPeer.m_characterID.m_userID, "ChatMessage",
+                        ZNet.instance.m_zdoMan.GetZDO(instanceMPeer.m_characterID).GetPosition(), 2,
+                        "SkipSleep",
+                        $"{count}/{allCharacterZdos.Count} sleeping ({Math.Round(sleepRatio * 100)} %)");*/
+                }
+            }
+        }
+
+        //SleepSkipPlugin.SleepSkipLogger.LogDebug($"Players sleeping: {count}");
+        //SleepSkipPlugin.SleepSkipLogger.LogDebug($"Ratio of players sleeping: {sleepRatio}");
+        //SleepSkipPlugin.SleepSkipLogger.LogDebug($"Threshold needed: {SleepSkipPlugin.ratio.Value}");
+
+        // If the ratio of the amount of players sleeping vs awake reaches the threshold, return true to sleep
+        if ((sleepRatio * 100) >= SleepSkipPlugin.ratio.Value)
+        {
+            SleepSkipPlugin.SleepSkipLogger.LogDebug(
+                $"SkipSleep: Threshold of {SleepSkipPlugin.ratio.Value} reached, sleeping...");
+            __result = true;
+            return false;
+        }
+
+        // Otherwise, result false
+        __result = false;
+
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(Menu), nameof(Menu.Start))]
+static class Menu_Start_Patch
+{
+    static void Postfix(Menu __instance)
+    {
+        SleepSkipPlugin.Dialog = UnityEngine.Object.Instantiate(Menu.instance.m_quitDialog.gameObject,
+            Hud.instance.m_rootObject.transform.parent.parent, true);
+        Button.ButtonClickedEvent noClicked = new();
+        noClicked.AddListener(onDeclineSleep);
+        SleepSkipPlugin.Dialog.transform.Find("dialog/Button_no").GetComponent<Button>().onClick = noClicked;
+        Button.ButtonClickedEvent yesClicked = new();
+        yesClicked.AddListener(onAcceptSleep);
+        SleepSkipPlugin.Dialog.transform.Find("dialog/Button_yes").GetComponent<Button>().onClick = yesClicked;
+    }
+
+    public static void onDeclineSleep()
+    {
+        SleepSkipPlugin.Dialog!.SetActive(false);
+        SleepSkipPlugin.acceptedSleepCount = 0;
+        // Notify everyone that they canceled sleep
+        ZRoutedRpc.instance.InvokeRoutedRPC(ZRoutedRpc.Everybody, "SleepStop", 2, "SkipSleepCanceled",
+            $"Sleep canceled by {Player.m_localPlayer.GetPlayerName()}");
+        // Send RPC to kick everyone from their bed
+    }
+
+    public static void onAcceptSleep()
+    {
+        //SleepSkipPlugin.Dialog!.SetActive(false);
+        SleepSkipPlugin.acceptedSleepCount += 1;
+
+        // Should update the value of how many accepted here.
+    }
+}
+
+[HarmonyPatch(typeof(Menu), nameof(Menu.IsVisible))]
+static class Menu_IsVisible_Patch
+{
+    static void Postfix(Menu __instance, ref bool __result)
+    {
+        if (SleepSkipPlugin.Dialog && SleepSkipPlugin.Dialog?.activeSelf == true)
+        {
+            SleepSkipPlugin.Dialog!.transform.Find("dialog/Exit").GetComponent<Text>().text =
+                $"{SleepSkipPlugin.acceptedSleepCount} people want to sleep. Do you?";
+            __result = true;
+        }
+    }
+}
+
+[HarmonyPatch(typeof(Game), nameof(Game.Start))]
+static class Game_Start_Patch
+{
+    static void Postfix(Game __instance)
+    {
+        // OpenMenu on the client
+        ZRoutedRpc.instance.Register("OpenMenuOnClient",SleepSkipPlugin.OpenMenuOnClient);
+
+        // Send client choice to the server
+        ZRoutedRpc.instance.Register<Vector3, int, string, string>("SleepSkipSendToServer",
+            new RoutedMethod<Vector3, int, string, string>.Method(this.RPC_ChatMessage));
+
+        // Update Clients that Accepted Value
+        ZRoutedRpc.instance.Register<Vector3, int, string, string>("UpdateSleepCount",
+            new RoutedMethod<Vector3, int, string, string>.Method(this.RPC_ChatMessage));
+    }
+}
+
+[HarmonyPatch(typeof(Player), nameof(Player.SetLocalPlayer))]
+static class SetCharacterId
+{
+    private static void Postfix(Player __instance)
+    {
+        ZNet.instance.m_characterID = __instance.GetZDOID();
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,154 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using ServerSync;
+using UnityEngine;
+
+namespace SleepSkip
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class SleepSkipPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "SleepSkip";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+
+        internal static string ConnectionError = "";
+
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource SleepSkipLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+        
+        public static GameObject? Dialog;
+        internal static int acceptedSleepCount = 0;
+
+        public enum Toggle
+        {
+            On = 1,
+            Off = 0
+        }
+
+        public void Awake()
+        {
+            _serverConfigLocked = config("1 - General", "Lock Configuration", Toggle.On,
+                "If on, the configuration is locked and can be changed by server admins only.");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+
+            ratio = config("1 - General", "Percent of players", 1.0,
+                new ConfigDescription(
+                    "Threshold of players that need to be sleeping.\nValues are in percentage 0% - 100%.",
+                    new AcceptableValueRange<double>(1, 100)));
+
+            message = config("1 - General", "showMessage", true,
+                "Show a continuous message of the amount of players currently sleeping (if > 0)");
+
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                SleepSkipLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                SleepSkipLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                SleepSkipLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+        internal void ResetVariables()
+        {
+            
+        }
+
+        internal static void OpenMenuOnClient(long senderId)
+        {
+            acceptedSleepCount = count;
+            Dialog!.SetActive(true);
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<Toggle> _serverConfigLocked = null!;
+        internal static ConfigEntry<double> ratio = null!;
+        internal static ConfigEntry<bool> message = null!;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            public bool? Browsable = false;
+        }
+
+        class AcceptableShortcuts : AcceptableValueBase
+        {
+            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
+            {
+            }
+
+            public override object Clamp(object value) => value;
+            public override bool IsValid(object value) => true;
+
+            public override string ToDescriptionString() =>
+                "# Acceptable values: " + string.Join(", ", KeyboardShortcut.AllKeyCodes);
+        }
+
+        #endregion
+    }
+}
